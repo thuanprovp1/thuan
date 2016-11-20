494401ef50891e5fbeaca492ff91fb711022fde2
thuanprovp1 <thuanprovp1@gmail.com>
2016-11-20 14:27:28 +0700

    fix product

--- app/src/admin/chart/chart.controller.js
+++ app/src/admin/chart/chart.controller.js
@@ -2,7 +2,55 @@
  * Created by Huong on 9/9/2016.
  */
 angular.module('myApp')
-    .controller('ChartController', function ($scope, TableHangHoa,DataTable) {
+    .controller('ChartController', function ($scope, TableHangHoa,DataTable,ProductService) {
+
+        $scope.edit = function (data) {
+            $scope.$apply(function () {
+                $scope.product = data;
+            });
+        };
+
+        //reset data khi nhập mới
+        $scope.addNew = function () {
+            $scope.product = {}
+        };
+
+        //xóa object
+        $scope.delete = function (data) {
+            $scope.$apply(function () {
+                $scope.product = data;
+            });
+            ProductService.deleteProduct($scope.product._id)
+                .then(function () {
+                    alert('Delete Success');
+                    angular.element('#product_table').DataTable().ajax.reload(null, false);
+                })
+                .catch(function (ex) {
+                    alert(err.toString())
+                })
+            ;
+        };
+        //khi click vào nút save
+        $scope.save = function (data) {
+
+            //kiểm tra có id ko,có thì update,ko thì create
+            var checkIdExit = ($scope.product._id ) ? ProductService.updateProduct : ProductService.createProduct;
+
+            checkIdExit.call(null, $scope.product)
+                .then(function () {
+                    alert('Success');
+                    angular.element('#productModal').modal('hide');
+                    angular.element('#product_table').DataTable().ajax.reload(null, false);
+
+                })
+                .catch(function (err) {
+                    alert("Data is invalid");
+                    console.log(err.toString);
+                })
+        };
+        
+        
+        
         TableHangHoa.fetchAllProducts()
             .then(function (response) {
                 $scope.products = response.data;
@@ -14,30 +62,44 @@ angular.module('myApp')
         $scope.refeshPage = function () {
             $router.reload();
         };
-        var options = {
-            url: 'http://localhost:8081/product/fetch',
-            columns: [
-                {'title': 'Id', 'data': '_id'},
-                {'title': 'Code', 'data': 'code'},
-                {'title': 'Name', 'data': 'name'},
-                {'title': 'Price', 'data': 'price'},
-                {'title': 'Category Code', 'data': 'category.code',"defaultContent":"Not available"},
-                {'title': 'Category Name', 'data': 'category.name',"defaultContent":"Not available"},
-                {'title': 'User Name', 'data': 'user.username',"defaultContent":"Not available"},
-                {'title': 'Password', 'data': 'user.password',"defaultContent":"Not available"},
-                {'title': 'Create Date', 'data': 'createdAt',"defaultContent":"Not available"},
-                {'title': 'Modifined Date', 'data': 'updatedAt',"defaultContent":"Not available"},
-                {'title': 'Action', 'data': null}
-            ],
-            columnDefs: [
-                {
-                    "render": function (data, type, row) {
-                        return '<button class="btn btn-danger" id="btn-delete"><i class="fa fa-trash-o" aria-hidden="true"></i></button>'
-                            + '<button class="btn btn-info"   id="btn-edit"  ><i class="fa fa-pencil" aria-hidden = "true"></i></button>'
-                    },
-                    "targets": 10
-                }
-            ]
+
+        var loadProduct = function () {
+            var options = {
+                url: 'http://localhost:8081/product/fetch',
+                columns: [
+                    {'title': 'Id', 'data': '_id'},
+                    {'title': 'Code', 'data': 'code'},
+                    {'title': 'Name', 'data': 'name'},
+                    {'title': 'Price', 'data': 'price'},
+                    {'title': 'Category Code', 'data': 'category.code', "defaultContent": "Not available"},
+                    {'title': 'Category Name', 'data': 'category.name', "defaultContent": "Not available"},
+                    {'title': 'User Name', 'data': 'user.username', "defaultContent": "Not available"},
+                    {'title': 'Password', 'data': 'user.password', "defaultContent": "Not available"},
+                    {'title': 'Create Date', 'data': 'createdAt', "defaultContent": "Not available"},
+                    {'title': 'Modifined Date', 'data': 'updatedAt', "defaultContent": "Not available"},
+                    {'title': 'Action', 'data': null}
+                ],
+                columnDefs: [
+                    {
+                        "render": function (data, type, row) {
+                            return '<button class="btn btn-danger" id="btn-delete"><i class="fa fa-trash-o" aria-hidden="true"></i></button>'
+                                + '<button class="btn btn-info"   id="btn-edit"  ' +
+                                'data-toggle="modal" data-target="#productModal" data-whatever="@mdo">' +
+                                '<i class="fa fa-pencil" aria-hidden = "true"></i></button>'
+                        },
+                        "targets": 10
+                    }
+                ]
+            };
+
+            options.delete = function (data) {
+                $scope.delete(data);
+            };
+            options.edit = function (data) {
+                $scope.edit(data);
+            };
+
+            DataTable.generateDataTable(options, angular.element('#product_table'));
         };
-        DataTable.generateDataTable(options, angular.element('#product_table'));
+        loadProduct();
     });
---
+++ app/src/admin/chart/chart.service.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Thuan on 11/20/2016.
+ */
+angular.module('myApp')
+    .service('ProductService', function ($http, API_URL) {
+        var url = [API_URL, 'product/'].join('');
+
+        this.updateProduct = function (data) {
+            return $http.post([url, 'update'].join(''), data)
+        };
+
+        this.createProduct = function (data) {
+            return $http.post([url, 'create'].join(''), data)
+        };
+
+        this.deleteProduct = function (data) {
+            return $http.get([url, 'delete?id=', data].join(''))
+        };
+    });
